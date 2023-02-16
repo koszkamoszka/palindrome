afca59c2856f9f1cec420c1a6d31a8fe58a4e933
koszkamoszka <laletina69@gmail.com>
2023-02-16 01:56:37 +0100

    add reading/writing to file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,36 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileAttribute;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        String inputFilePath = "C:\\Projects\\Java\\palindrome\\src\\main\\resources\\input.txt";
+        String outputFilePath = "C:\\Projects\\Java\\palindrome\\src\\main\\resources\\output.txt";
+        Scanner scanner = new Scanner(new File(inputFilePath));
         ArrayList<String> list = new ArrayList<>();
-        String str = scanner.nextLine();
+        String str;
 
-        while (!str.isEmpty()) {
-            list.add(str);
+        while (scanner.hasNextLine()) {
             str = scanner.nextLine();
+            list.add(str);
         }
 
+        Path outputFile = Path.of(outputFilePath);
+        Files.createFile(outputFile);
         for (String line : list) {
             if (isPalindrome2(line)) {
-                System.out.println(line);
+                Files.writeString(outputFile, line + "\n", StandardOpenOption.APPEND);
             }
         }
+        scanner.close();
     }
 
     private static boolean isPalindrome(String line) {
@@ -38,16 +50,28 @@ public class Main {
         return true;
     }
 
-    private static int method(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.println(i + j);
-            }
-        }
-    }
+//    public void whenReadSmallFileJava7_thenCorrect()
+//            throws IOException {
+//        String expected_value = "Hello, world!";
+//
+//        Path path = Paths.get("src/test/resources/fileTest.txt");
+//
+//        String read = Files.readAllLines(path).get(0);
+//        assertEquals(expected_value, read);
+//    }
+
+//    private static int method(int n) {
+//        int[][] array = new int[n][n];
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                array[i][j] = i + j;
+//            }
+//        }
+//    }
 }
 
 /*
+1 = const = nice
 log(n) = 16 ms = 0.016 s
 sqrt(n) = 316 ms = 0.316 s
 n / 2 = 50 000 ms = 50 s
